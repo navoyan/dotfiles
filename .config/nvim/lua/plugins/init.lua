59d56ec4cd9482c43be44562cbb3c13db51468d5
navoyan <narek.navoyan.05@gmail.com>
2024-12-08 21:16:17 +0400

    nvim: add obsidian.nvim plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -41,4 +41,31 @@ return {
             { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
         },
     },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*",
+        lazy = true,
+        ft = "markdown",
+        keys = {
+            { "<leader>of", "<cmd>ObsidianQuickSwitch<cr>" },
+            { "<leader>ot", "<cmd>ObsidianToday<cr>" },
+            { "<leader>od", "<cmd>ObsidianDailies<cr>" },
+            { "<leader>ow", "<cmd>ObsidianWorkspace<cr>" },
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "work",
+                    path = "~/vaults/work",
+                },
+                {
+                    name = "personal",
+                    path = "~/vaults/personal",
+                },
+            },
+        },
+    },
 }
