3bb01c96526872e3545175619e6950fae165dfe6
navoyan <narek.navoyan.05@gmail.com>
2024-12-08 21:19:54 +0400

    experimenting with kitty terminal

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -27,6 +27,7 @@ return {
     },
     {
         "christoomey/vim-tmux-navigator",
+        enabled = false,
         cmd = {
             "TmuxNavigateLeft",
             "TmuxNavigateDown",
@@ -67,5 +68,16 @@ return {
                 },
             },
         },
+    },
+    {
+        "mikesmithgh/kitty-scrollback.nvim",
+        lazy = true,
+        cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+        event = { "User KittyScrollbackLaunch" },
+        -- version = '*', -- latest stable version, may have breaking changes if major version changed
+        -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+        config = function()
+            require("kitty-scrollback").setup()
+        end,
     },
 }
