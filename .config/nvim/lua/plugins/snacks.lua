7e80970673d7752729734c3551bc4372f4995eb3
navoyan <narek.navoyan.05@gmail.com>
2025-03-10 06:28:47 +0400

    nvim: use `--remote` instead of `--remote-tab` in `snacks.lazygit`

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -196,6 +196,16 @@ return {
                 enabled = false,
             },
         },
+        lazygit = {
+            config = {
+                os = {
+                    edit = '[ -z "[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
+                    editAtLine = ': `[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" &&  nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
+                    editAtLineAndWait = "nvim +{{line}} {{filename}}",
+                    openDirInEditor = '[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})',
+                },
+            },
+        },
         bigfile = {},
         quickfile = {},
         gitbrowse = {
