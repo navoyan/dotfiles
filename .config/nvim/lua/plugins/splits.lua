9782181b4b583a636d158e31ad386e4962d29656
navoyan <narek.navoyan.05@gmail.com>
2025-04-10 16:50:24 +0400

    kitty: tweak into usable state and setup scrollback

--- .config/nvim/lua/plugins/splits.lua
+++ .config/nvim/lua/plugins/splits.lua
@@ -2,6 +2,7 @@ return {
     {
         "mrjones2014/smart-splits.nvim",
         lazy = false,
+        build = "./kitty/install-kittens.bash",
         config = function()
             local splits = require("smart-splits")
 
@@ -11,15 +12,15 @@ return {
             vim.keymap.set("n", "<leader>sk", "<cmd>horizontal topleft split<cr>")
             vim.keymap.set("n", "<leader>sl", "<cmd>vertical rightbelow split<cr>")
             -- moving between splits
-            vim.keymap.set("n", "<A-h>", splits.move_cursor_left)
-            vim.keymap.set("n", "<A-j>", splits.move_cursor_down)
-            vim.keymap.set("n", "<A-k>", splits.move_cursor_up)
-            vim.keymap.set("n", "<A-l>", splits.move_cursor_right)
+            vim.keymap.set("n", "<M-h>", splits.move_cursor_left)
+            vim.keymap.set("n", "<M-j>", splits.move_cursor_down)
+            vim.keymap.set("n", "<M-k>", splits.move_cursor_up)
+            vim.keymap.set("n", "<M-l>", splits.move_cursor_right)
             -- resizing splits
-            vim.keymap.set("n", "<C-Space>r<Left>", splits.resize_left)
-            vim.keymap.set("n", "<C-Space>r<Down>", splits.resize_down)
-            vim.keymap.set("n", "<C-Space>r<Up>", splits.resize_up)
-            vim.keymap.set("n", "<C-Space>r<Right>", splits.resize_right)
+            vim.keymap.set("n", "<M-S-h>", splits.resize_left)
+            vim.keymap.set("n", "<M-S-j>", splits.resize_down)
+            vim.keymap.set("n", "<M-S-k>", splits.resize_up)
+            vim.keymap.set("n", "<M-S-l>", splits.resize_right)
         end,
     },
     {
@@ -38,17 +39,5 @@ return {
             { "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
             { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
         },
-    },
-    {
-        "mikesmithgh/kitty-scrollback.nvim",
-        enabled = false,
-        lazy = true,
-        cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
-        event = { "User KittyScrollbackLaunch" },
-        -- version = '*', -- latest stable version, may have breaking changes if major version changed
-        -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-        config = function()
-            require("kitty-scrollback").setup()
-        end,
     },
 }
