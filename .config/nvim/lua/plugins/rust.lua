6bd9bfce8b564335ee18aac82aa5177666999b9e
navoyan <narek.navoyan.05@gmail.com>
2025-07-13 06:54:27 +0400

    nvim: bump `rustaceanvim`, allow changing check command without restarting editor

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -1,13 +1,49 @@
 return {
     {
         "mrcjkb/rustaceanvim",
-        version = "^4", -- Recommended
+        version = "*",
         ft = { "rust" },
         opts = {
             -- Plugin configuration
             tools = {},
             -- LSP configuration
             server = {
+                on_attach = function(_, bufnr)
+                    local ra_client_name = "rust-analyzer"
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    map("n", "<leader>cL", function()
+                        local check_cmd = vim.tbl_get(
+                            vim.lsp.config,
+                            "rust-analyzer",
+                            "settings",
+                            "rust-analyzer",
+                            "check",
+                            "command"
+                        ) or "check"
+
+                        check_cmd = check_cmd == "check" and "clippy" or "check"
+
+                        -- NOTE: rustaceanvim extends `opts.server.default_settings`
+                        -- with `vim.lsp.config[ra_client_name].default_settings`
+                        vim.lsp.config(ra_client_name, {
+                            default_settings = {
+                                ["rust-analyzer"] = {
+                                    check = {
+                                        command = check_cmd,
+                                    },
+                                },
+                            },
+                        })
+
+                        vim.cmd.RustAnalyzer("restart")
+                    end)
+                end,
                 default_settings = {
                     -- rust-analyzer language server configuration
                     ["rust-analyzer"] = {
@@ -45,6 +81,7 @@ return {
         tag = "stable",
         event = { "BufRead Cargo.toml" },
         opts = {
+            enable_update_available_warning = false,
             completion = {
                 crates = {
                     enabled = true,
