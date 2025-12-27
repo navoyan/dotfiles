925e78a8f5a08f97d1ca9b0dcbe46657852e9236
navoyan <narek.navoyan.05@gmail.com>
2025-12-27 21:42:11 +0400

    nvim: disable treesitter/lsp-based folding

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -14,19 +14,3 @@ opt.inccommand = "split" -- Show partial off-screen `:substitute` results in a p
 
 opt.fillchars:append({ diff = "╱" }) -- Replace character for visualizing deleted lines in diff-mode
 opt.foldtext = "" -- Display the folded line with regular (treesitter) highlighting
-
-opt.foldmethod = "expr" -- Use 'foldexpr' for folding
-opt.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use treesitter folding
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client == nil then
-            return
-        end
-
-        if client:supports_method("textDocument/foldingRange") then
-            vim.opt_local.foldexpr = "v:lua.vim.lsp.foldexpr()" -- Use LSP folding
-        end
-    end,
-})
