3da82a8b2d51b629914a1f1998b7df991909e7d8
navoyan <narek.navoyan.05@gmail.com>
2026-04-13 05:00:15 +0400

    nvim: migrate to nvim-treesitter main branch

--- .config/nvim/plugin/30_third_party/treesitter.lua
+++ .config/nvim/plugin/30_third_party/treesitter.lua
@@ -2,47 +2,48 @@ local schedule, config = require("schedule"), require("config")
 
 schedule.now_if_args(function()
     vim.pack.add({
-        {
-            src = config.github("nvim-treesitter/nvim-treesitter"),
-            version = "master",
-        },
+        config.github("nvim-treesitter/nvim-treesitter"),
     })
 
     config.on_packchanged("nvim-treesitter", { "install", "update" }, vim.cmd.TSUpdate)
 
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-            "rust",
-            "python",
-            "bash",
-            "c",
-            "html",
-            "lua",
-            "luadoc",
-            "markdown",
-            "vim",
-            "vimdoc",
-        },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-            -- When experiencing weird indenting issues, the language needs to be added
-            -- to the list of additional_vim_regex_highlighting and disabled languages for indent.
-            additional_vim_regex_highlighting = { "ruby" },
-        },
-        indent = { enable = false, disable = { "ruby" } },
-    })
+    local treesitter = require("nvim-treesitter")
 
-    vim.filetype.add({
-        extension = {
-            gotmpl = "gotmpl",
-        },
-        pattern = {
-            [".*/templates/.*%.tpl"] = "helm",
-            [".*/templates/.*%.ya?ml"] = "helm",
-            ["helmfile.*%.ya?ml"] = "helm",
-        },
-    })
+    ---@type table<string, string>
+    local installed_for_fts = {}
+
+    ---@param lang string
+    local function mark_fts_as_installed_for_lang(lang)
+        local fts = vim.treesitter.language.get_filetypes(lang)
+        for _, ft in ipairs(fts) do
+            installed_for_fts[ft] = lang
+        end
+    end
+
+    for _, lang in ipairs(treesitter.get_installed("parsers")) do
+        mark_fts_as_installed_for_lang(lang)
+    end
+
+    local available_parsers = require("nvim-treesitter.parsers")
+
+    config.new_autocmd("FileType", "*", function(event)
+        local buf = event.buf
+        local ft = event.match
+
+        local installed_lang = installed_for_fts[ft]
+        if installed_lang then
+            vim.treesitter.start(buf, installed_lang)
+            return
+        end
+
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+        if not available_parsers[lang] then
+            return
+        end
+
+        treesitter.install(lang):await(function()
+            vim.treesitter.start(buf, lang)
+            mark_fts_as_installed_for_lang(lang)
+        end)
+    end)
 end)
