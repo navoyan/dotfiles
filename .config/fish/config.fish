72b187acb0ca83fe5eb09b6882495fd375e8bacf
navoyan <narek.navoyan.05@gmail.com>
2026-08-29 20:12:57 +0400

    fish: add keybinds and abbrebiations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,49 @@
 set -gx SHELL $(which fish)
 
-set fish_function_path "$__fish_config_dir/user_functions" $fish_function_path
+set -g fish_greeting ""
+set -g fish_key_bindings fish_vi_key_bindings
 
+set fish_function_path "$__fish_config_dir/user_functions" $fish_function_path
+set fish_complete_path $fish_complete_path "/usr/share/fish/completions"
 
 abbr v nvim
+abbr se sudoedit
 
 abbr g git
 abbr b bazel
 
-abbr k kubectl
-abbr kns kubens
+abbr k    kubectl
+abbr kns  kubens
 abbr kctx kubectx
 
 abbr rust evcxr
 
 alias page "command $PAGER"
-abbr p page
+abbr  p     page
 
-bind -M insert ctrl-backspace backward-kill-word
-bind -M insert ctrl-enter 'commandline -i \\n' expand-abbr
+function netbird-switch -a profile
+    netbird profile select $profile
+    netbird up
+end
+abbr ns netbird-switch
+abbr n netbird
 
-bind -M visual y fish_clipboard_copy
-bind -m visual V beginning-of-line begin-selection end-of-line force-repaint
+function fish_user_key_bindings
+    bind -M insert ctrl-backspace backward-kill-word
+    bind -M insert ctrl-enter     'commandline -i \\n' expand-abbr
 
-zoxide init fish --cmd cd | source
+    bind                         yy fish_clipboard_copy
+    bind -M visual -m default -s y  "fish_clipboard_copy; commandline -f end-selection repaint-mode"
+
+    bind --erase --preset s
+    bind                  sl __fish_vi_delete_char
+
+    bind -m visual x begin-selection
+    bind -m visual X beginning-of-line begin-selection end-of-line force-repaint
+
+    bind m repeat-jump
+    bind -M visual m repeat-jump
+end
+
+zoxide init fish --cmd j | source
 fzf --fish | source
