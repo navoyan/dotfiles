9f8aedfd164869f8e3a02d656aeb7d470abb7e46
navoyan <narek.navoyan.05@gmail.com>
2026-09-05 07:10:55 +0400

    nvim: use mini.misc for lazy-loading

--- config/npage/plugin/30_third_party/kitty_scrollback.lua
+++ config/npage/plugin/30_third_party/kitty_scrollback.lua
@@ -1,7 +1,7 @@
 local schedule, config = require("schedule"), require("config")
 local map = vim.keymap.set
 
-schedule.now(function()
+schedule.on_event("User~KittyScrollbackLaunch", function()
     vim.pack.add({
         {
             src = config.github("mikesmithgh/kitty-scrollback.nvim"),
@@ -9,14 +9,12 @@ schedule.now(function()
         },
     })
 
-    config.new_autocmd("User", "KittyScrollbackLaunch", function()
-        require("kitty-scrollback").setup({
-            {
-                paste_window = { yank_register = "x" },
-            },
-        })
+    require("kitty-scrollback").setup({
+        {
+            paste_window = { yank_register = "x" },
+        },
+    })
 
-        -- Yank into paste window
-        map("n", "x", [["xy]])
-    end)
+    -- Yank into paste window
+    map("n", "x", [["xy]])
 end)
